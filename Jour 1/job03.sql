5f96fc79adf7bd2bdf6466dda9c30e1d087e459d
domenico-mandolino <146713339+domenico-mandolino@users.noreply.github.com>
2024-01-30 16:27:46 +0100

    Update job03.sql

---
+++ Jour 1/job03.sql
@@ -0,0 +1,16 @@
+MariaDB [(none)]> CREATE TABLE LaPlateforme.eutidaint ( id INT, nom VARCHAR(225), prenom VARCHAR(25), age INT, email VARCHAR(255));
+Query OK, 0 rows affected (0,024 sec)
+
+MariaDB [(none)]> SHOW TABLES FROM LaPlateforme;
++------------------------+
+| Tables_in_LaPlateforme |
++------------------------+
+| eutidaint              |
++------------------------+
+1 row in set (0,003 sec)
+
+MariaDB [(none)]> 
+
+
+MariaDB [(none)]> Ctrl-C -- exit!
+Aborted
